a46958764f3dae1128b8c708c01d897fd3aa113d
Annie Nusbaum <annie.nusbaum.83@gmail.com>
2023-12-08 12:38:59 -0500

    javascript

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Annie";
+let lastName = "Nusbaum";
+var age = 31;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+
+//bonus
+
+fullName = `${firstName} ${lastName}`;
+console.log(fullName);
 
 // Exercise 3
+let city = "Tampa";
+let hobby = "reading";
+let myStory = `Hey! My name is ${fullName}. I live in ${city}. I love to ${hobby} and going to the gym and coding of course!`;
+
+console.log(myStory);
+
+/Users/annienusbaum/Desktop/repos/JavaScript_Variables/app.js
